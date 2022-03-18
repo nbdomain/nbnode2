3c62e5ecd2e92c22f3a1b99f2af0c7bb66b32686
bloodchen <bloodchen@gmail.com>
2022-03-18 13:48:20 +0800

    update

--- core/default_config.js
+++ core/default_config.js
@@ -29,17 +29,17 @@ const CONFIG = {
   debug: true,
   "node_info": {
     nbdomain: "", //nbdomain of the node
-    payment:{bsv:"1Bti24c8ZQLYMTaifVkiRwJ1cwT4K6ucJu",ar:""}, //address to receive payment
+    payment:{bsv:"1Bti24c8ZQLYMTaifVkiRwJ1cwT4K6ucJu",ar:"gOyqCZBB-JmX1eDcYrIPPV71msTBBzPKwnEF3oEB-ZQ"}, //address to receive payment
     domain: "", //domain name or IP of the node
     contact: "", //contact email of the owner,required for ssl certificate
     https:false, //for auto generated SSL certificate
     port:9000,  //port for http service
     prices: {
       domainHost:  {bsv:1000,ar:1000}, //host user's triditional domain and link to a nbdomain
-      keyUpdate: {bsv:1000,ar:1000}
+      keyUpdate: {bsv:0.5,ar:1.0}
     }
   },
-  peers:[],//other(than public) nbnode peers (optional)
+  peers:["https://tnode.nbdomain.com"],//other(than public) nbnode peers (optional)
   "exit_count": 0, //exit the process each x minutes. Used with PM2 to restart process every x minutes. 0 for no exit
   "proxy_map": {
     "/api/": "api",
