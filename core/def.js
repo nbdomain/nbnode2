fbf827d9803659cbb45481d11a10a5da964df504
bloodchen <bloodchen@gmail.com>
2022-07-13 07:18:06 +0800

    update

--- core/def.js
+++ core/def.js
@@ -40,7 +40,7 @@ const DEF = {
 
     BLOCK_VER: 1,       //block format
     BLOCK_TIME: 5000,   //consense time
-    CONSENSUE_COUNT: 2, //number of nodes needed a node connects to
+    CONSENSUE_COUNT: 3, //number of nodes needed a node connects to
     MAX_BLOCK_LENGTH: 100 //max transaction in a block
 }
 
