0df096ef249c5e6292a9e750ab1f09cbc08bd58b
bloodchen <bloodchen@gmail.com>
2022-07-13 08:05:06 +0800

    update

--- core/blockManager.js
+++ core/blockManager.js
@@ -32,6 +32,7 @@ class BlockMgr {
         }
         const merkel = await this.computeMerkel(txs)
         const block = { version: DEF.BLOCK_VER, height: height, merkel, txs, preHash: preBlock ? preBlock.hash : null }
+        console.log(block)
         block.hash = await Util.dataHash(stringify(block))
         console.log("new block created. hash:", block.hash)
         return block
