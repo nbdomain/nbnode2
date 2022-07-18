cfca9bc9be1d7ff6a9a3c6d685cbf5cb57f9810a
bloodchen <bloodchen@gmail.com>
2022-07-18 11:14:58 +0800

    update

--- core/def.js
+++ core/def.js
@@ -38,7 +38,7 @@ const DEF = {
     TX_INVALIDTX: 1, //MARK an invalid tx
     TX_FORMAT2: 2,
 
-    BLOCK_VER: 6,       //block format
+    BLOCK_VER: 7,       //block format
     BLOCK_TIME: 5000,   //consense time
     CONSENSUE_COUNT: 3, //number of nodes needed a node connects to
     MAX_BLOCK_LENGTH: 100 //max transaction in a block
