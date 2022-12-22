c003e9c5aee75ce84c29e39ed916ae67d331a348
bloodchen <bloodchen@gmail.com>
2022-12-22 15:25:52 +0800

    update

--- npmUpdate.sh
+++
@@ -1,4 +0,0 @@
-curl "https://tnode.nbdomain.com/api/admin?key=kkk111&cmd=pnpm"
-curl "https://api.nbdomain.com/api/admin?key=^kkk^OpoP&cmd=pnpm"
-curl "http://143.198.66.116:9100/api/admin?key=NodeAdminQWT&cmd=pnpm"
-curl "https://nbnode.maxthon.com/api/admin?key=NodeAdminQWT&cmd=pnpm"
--- upallnodes.sh
+++
@@ -1,4 +0,0 @@
-curl "https://tnode.nbdomain.com/api/admin?key=kkk111&cmd=update"
-curl "https://api.nbdomain.com/api/admin?key=^kkk^OpoP&cmd=update"
-curl "http://143.198.66.116:9100/api/admin?key=NodeAdminQWT&cmd=update"
-curl "https://nbnode.maxthon.com/api/admin?key=NodeAdminQWT&cmd=update"
