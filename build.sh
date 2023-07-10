86d089742b776ebaf9add657ce478a9c9c69910c
bloodchen <bloodchen@gmail.com>
2023-07-10 08:48:29 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 ##docker buildx build . --platform linux/amd64,linux/arm64,linux/arm/v7 -t bloodchen/nbdb --push
-docker container stop nbdb
-docker container rm nbdb
 docker build -t nbdb .
 mkdir data && chmod a+rw data
-docker run --name nbdb -p 9000:9000 -v $(pwd)/data:/home/node/app/data --restart=always -d nbdb
+docker run --name nbdb -p 9000:9000 -v $(pwd)/data:/home/node/app/data --restart=always -d nbdb --rm
