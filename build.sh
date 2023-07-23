2f2830f42b3f595720597a31ce6f4ac46f16dae3
bloodchen <bloodchen@gmail.com>
2023-07-23 12:10:30 +0800

    update

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ docker container stop nbdb
 docker container rm nbdb
 docker build -t nbdb .
 mkdir data && chmod a+rw data
-docker run --name nbdb -p 9000:9000 -v $(pwd)/data:/home/node/app/data --restart=always -d nbdb
+docker run --name nbdb -p 9100:9000 -v $(pwd)/data:/home/node/app/data --restart=always -d nbdb
