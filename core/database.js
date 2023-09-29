4badc99c11311f26ced2cec24be8368570fa1165
bloodchen <bloodchen@gmail.com>
2023-09-29 17:28:18 +0800

    update

--- core/database.js
+++ core/database.js
@@ -1659,6 +1659,11 @@ class Database {
     }
   }
   saveRawItem(item, type = 'keys') {
+    let keyname = 'key'
+    if (type === 'domains') {
+      keyname = 'domain'
+    }
+    console.log("saving:", item[keyname])
     if (type === 'keys') {
       const sql = `Insert or Replace into keys (key,value,domain,ts,parent,
         p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,u1,u2,u3,u4,u5) 
