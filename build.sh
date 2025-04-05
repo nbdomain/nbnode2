4da2c0f514dae3557655524e33f8f9174706b790
bloodchen <bloodchen@gmail.com>
2025-04-05 19:48:19 +0900

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ NAME="nbdb_${chainid}"
 PORT="${dockerPort:=9100}"
 docker container stop $NAME
 docker container rm $NAME
-docker build -t $NAME .
+docker build --no-cache -t $NAME .
 mkdir data && chmod a+rw data
 docker run --name $NAME --ulimit nofile=90000:90000 -p $PORT:9000 \
 --log-driver json-file --log-opt max-size=200m --log-opt max-file=3 \
